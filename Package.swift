1439939ce57c5c5295224f2ec14b199fce876c40
asynclet <a.vaganova@sports.ru>
2025-04-30 01:35:27 +0300

    Updated package else

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
             targets: [
                 "DBDebugToolkitSwift",
                 "DBDebugToolkitObjC",
-                "DBDebugToolkitResources",
                 "DBDebugToolkitCommon"
             ]
         ),
